e19b45d62b5e6b2493b4c62f08714fb47e46d07f
sam06tanb <samirtajra53@gmail.com>
2024-08-13 17:00:09 -0300

    Added DaoFactory and SellerDaoJDBC

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,6 +1,8 @@
 package application;
 import application.data.ConnectionJDBC;
 import db.DB;
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 import java.sql.Connection;
@@ -13,6 +15,8 @@ public class Main {
         Department o = new Department(1, "Books");
 
         Seller seller = new Seller(21, "Bob", "Bob@gmail.com", new Date(), 3000.0, o);
+
+        SellerDao sellerDao = DaoFactory.createSellerDao();
 
         System.out.println(o);
         System.out.println(seller);
