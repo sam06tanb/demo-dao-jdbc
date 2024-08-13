96e057ff19ad29af13a7d9869bd96038c11828db
sam06tanb <samirtajra53@gmail.com>
2024-08-13 20:10:23 -0300

    Insert implementation

--- src/application/Main.java
+++ src/application/Main.java
@@ -6,9 +6,9 @@ import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
-import javax.swing.plaf.basic.BasicInternalFrameTitlePane;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.Date;
 import java.util.List;
 
 
@@ -23,32 +23,29 @@ public class Main {
 
             SellerDao sellerDao = DaoFactory.createSellerDao(conn);
 
-            System.out.println("=== TEST 1: seller findById ====");
-            Seller seller = sellerDao.findById(3);
-            System.out.println(seller);
 
-            System.out.println("\n=== TEST 2: seller findByDepartment ====");
+            System.out.println("\n=== TEST 2: seller findByDepartment =====");
             Department department = new Department(2, null);
             List<Seller> list = sellerDao.findByDepartment(department);
             for (Seller obj : list) {
                 System.out.println(obj);
-
-            System.out.println("\n=== TEST 3: seller findAll ====");
-            list = sellerDao.findAll();
-            for (Seller ob : list) {
-                System.out.println(ob);
-            }
             }
 
+            System.out.println("\n=== TEST 4: seller insert =====");
+            Seller newSeller = new Seller(null, "Bryan", "Bryan@gmail.com", new Date(), 5000.0, department);
+            sellerDao.insert(newSeller);
+            System.out.println("Inserted! New id = " + newSeller.getId());
         }
-        catch (SQLException e) {
-            e.getMessage();
+        catch (SQLException ex) {
+            throw new RuntimeException(ex);
         }
-        catch (ClassNotFoundException e) {
-            throw new RuntimeException(e);
+        catch (ClassNotFoundException ex) {
+            throw new RuntimeException(ex);
         }
         finally {
             DB.closeConnection();
         }
     }
 }
+
+
