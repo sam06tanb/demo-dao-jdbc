cd7e2fedeea2a50b7eb6b74a62dad59c3e766a92
sam06tanb <samirtajra53@gmail.com>
2024-08-13 20:11:34 -0300

    Insert implementation + Test1 + Test 3

--- src/application/Main.java
+++ src/application/Main.java
@@ -24,9 +24,19 @@ public class Main {
             SellerDao sellerDao = DaoFactory.createSellerDao(conn);
 
 
+            System.out.println("=== TEST 1: seller findById =====");
+            Seller seller = sellerDao.findById(3);
+            System.out.println(seller);
+
             System.out.println("\n=== TEST 2: seller findByDepartment =====");
             Department department = new Department(2, null);
             List<Seller> list = sellerDao.findByDepartment(department);
+            for (Seller obj : list) {
+                System.out.println(obj);
+            }
+
+            System.out.println("\n=== TEST 3: seller findAll =====");
+            list = sellerDao.findAll();
             for (Seller obj : list) {
                 System.out.println(obj);
             }
